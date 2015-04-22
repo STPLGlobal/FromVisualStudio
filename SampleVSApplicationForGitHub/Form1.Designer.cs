fc30c18d2ad1b45788b28150211003d8b78e2380
Rajesh Pandurangan <raajesh.rp@gmail.com>
2015-04-22 14:49:30 +0530

    Update Form1.Designer.cs
    Textbox name updated from Github site

--- SampleVSApplicationForGitHub/Form1.Designer.cs
+++ SampleVSApplicationForGitHub/Form1.Designer.cs
@@ -44,7 +44,7 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(17, 59);
-            this.textBox1.Name = "textBox1";
+            this.textBox1.Name = "txtName";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
             // 
